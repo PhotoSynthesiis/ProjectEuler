f132b8446951504644311290992b92ee08f8fc06
Qiushi Li <qsli@thoughtworks.com>
2013-02-10 22:40:23 +0800

    finished problem 11

---
+++ src/problems/Problem_12.java
@@ -0,0 +1,11 @@
+package problems;
+
+public class Problem_12 {
+    public static void main(String[] args) {
+        boolean shouldBreak = false;
+        int triangleLimit = 1;
+        while (!shouldBreak) {
+//            int triangleNumbers =
+        }
+    }
+}
---
+++ src/problems/test.java
@@ -0,0 +1,9 @@
+package problems;
+
+public class test {
+    public static void main(String[] args) {
+        int[][] a = new int[6][4];
+
+        System.out.println(a.length);
+    }
+}
