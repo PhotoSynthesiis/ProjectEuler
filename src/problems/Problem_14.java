0aca12816fbd7c3df437ecf99cc0e5a4be24aadb
Qiushi Li <qsli@thoughtworks.com>
2013-05-11 01:29:28 +0800

    finished 14

---
+++ src/problems/Problem_14.java
@@ -0,0 +1,28 @@
+package problems;
+
+public class Problem_14 {
+    public static void main(String[] args) {
+        int chainLength;
+        int maxChain = 0;
+        int MaxNum = 0;
+        for (int i = 2; i < 1000000; i++) {
+            long a = i;
+            int count = 0;
+            while (a != 1) {
+                if (a % 2 == 0) {
+                    a = a / 2;
+                } else {
+                    a = 3 * a + 1;
+                }
+                count++;
+            }
+            chainLength = count;
+            if (chainLength > maxChain) {
+                maxChain = chainLength;
+                MaxNum = i;
+            }
+        }
+        System.out.println(MaxNum + " " + maxChain);
+    }
+
+}
--- src/problems/test.java
+++ src/problems/test.java
@@ -1,57 +1,42 @@
 package problems;
 
+import java.io.*;
+
 public class test {
-    public static void main(String[] args) {
-        String a = "371072875312346376937673418348422520464";
-        Integer convertedInteger[][] = new Integer[3][13];
-        convertStringToInteger(a, convertedInteger);
-        int sumOfOneColumn, carryBitOfOneColumn = 0, remainOfOneColumn = 0;
-//        3710728753123
-//        4637693767341
-//        8348422520464
-//        16696845040928
-        int[] temp = new int[52];
-        for (int i = convertedInteger[1].length; i > 0; i--) {
-            temp[i + 1] = convertedInteger[0][i - 1];
-        }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        A a1 = new A();
+        Object a2 = null;
+//
+        a1.setP(111);
+//        a2 = a1;
+//        System.out.println(a2.getP());
+//        a1.setP(100);
+//        System.out.println(a2.getP());
 
-        int sumOfFirstTwo;
 
-        for (int i = 0; i < convertedInteger.length - 1; i++) {
-            for (int j = convertedInteger[1].length - 1; j >= 0; j--) {
-                sumOfOneColumn = temp[j + 2] + convertedInteger[i + 1][j];
-                remainOfOneColumn = sumOfOneColumn;
-                carryBitOfOneColumn = 0;
-                System.out.println(temp[j + 2] + " : " + convertedInteger[i + 1][j]);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(a1);
+        objectOutputStream.flush();
+        objectOutputStream.close();
 
-                if (sumOfOneColumn > 9) {
-                    carryBitOfOneColumn = 1;
-                    remainOfOneColumn = sumOfOneColumn - 10;
-                }
-                temp[j + 2] = remainOfOneColumn;
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
 
-                sumOfFirstTwo = temp[j + 1] + carryBitOfOneColumn;
-                temp[j + 1] += carryBitOfOneColumn;
-                if (sumOfFirstTwo > 9) {
-                    temp[j + 1] = sumOfFirstTwo - 10;
-                    temp[j]++;
-                }
-            }
-        }
+        a2 = objectInputStream.readObject();
+        A newA2 = (A) a2;
+        System.out.println(newA2.getP());
+    }
+}
 
-        for (int i = 0; i < temp.length; i++) {
-            System.out.print(temp[i]);
-        }
+class A implements Serializable {
+    private int p = 0;
 
+    public void setP(int i) {
+        p = i;
     }
 
-    private static void convertStringToInteger(String a, Integer[][] convertedInteger) {
-        int index = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 13; j++) {
-                convertedInteger[i][j] = Integer.parseInt(String.valueOf(a.charAt(index)));
-                index++;
-            }
-        }
+    public int getP() {
+        return p;
     }
 }
